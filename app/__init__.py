f9ff7ac25b64659ba2071fd122d1e1730895c9cb
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2021-10-27 15:35:47 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,3 @@
 # SoftDev
 # P00
 # 2021-10-27
-
-from os import urandom
-from flask import Flask
-
-app = Flask(__name__)
-
-# session key - 32 random bytes
-app.secret_key = urandom(32)
-
-from app import routes
-
-app.run()
