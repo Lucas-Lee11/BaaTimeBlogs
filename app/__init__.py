c1f6ba839f4815610c8419c102cf62db539ad5b3
AAWorks <axalonso12@gmail.com>
2021-10-29 09:17:11 -0400

    starter html/python for homepage and login

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,19 @@
 # SoftDev
 # P00
 # 2021-10-27
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST']) #, methods=['GET', 'POST'])
+def index():
+    """
+    create the basic login page, if session active auto-sign in
+    """
+    return render_template('homepage.html', username="user1")
+
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run() 
