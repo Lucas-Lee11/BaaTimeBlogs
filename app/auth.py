bf993edfd6bef287ff2171db7c15e63dad9589a8
tfahey20 <tfahey20@stuy.edu>
2021-11-04 08:40:09 -0400

    Added pseudocode for auth.py

--- app/auth.py
+++ app/auth.py
@@ -11,12 +11,19 @@ def hash(password: str) -> str:
     """
     return hashlib.sha512(password.encode()).hexdigest()
 
-def validate_new_user():
+def validate_new_user(user):
+	rows = cursor.execute("SELECT user FROM db").fetchall()
+	for i in rows:
+		if (i == user):
+			return false
+	return true
+		
     """
     validates inputs for creating a new user
     """
-
-def crt_user():
+	
+def crt_user(user, pw):
+	cursor.execute("INSERT INTO db VALUES('" + user +"','" + pw + "',NULL");
     """
     if all inputs pass, create new user with inputs
     """
@@ -28,7 +35,14 @@ def get_userid():
     """
     pass
 
-def auth_user():
+def auth_user(user,pw):
+	rows = cursor.execute("SELECT user, pass FROM db").fetchall()
+	up = "('" + user +"', '" + pw + "')")
+	for i in rows:
+		if (i[0] == up):
+			return true
+	return false
+		
     """
     authenticates user based off of username & pwd
     """
